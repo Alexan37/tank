bb882ec15d714446fe7658308f2ef9134c2c169f
Alexan37 <ansuprun98@mail.ru>
2025-05-06 23:09:43 +0800

    Shot of several shells

--- src/com/dong/tank/Tank.java
+++ src/com/dong/tank/Tank.java
@@ -1,3 +1,4 @@
+Tank.java:
 import java.awt.*;
 import java.awt.event.*;
 
@@ -12,7 +13,7 @@ public class Tank {
     private Direction dir = Direction.STOP;
     private Direction ptDir = Direction.D;
 
-    private TankClient tc;
+    TankClient tc = null;
 
     enum Direction {L, LU, U, RU, R, RD, D, LD, STOP}
 
@@ -32,36 +33,35 @@ public class Tank {
         g.fillOval(x, y, WIDTH, HEIGHT);
         g.setColor(c);
 
-        // Рисуем ствол танка в зависимости от направления
-        int centerX = x + WIDTH / 2;
-        int centerY = y + HEIGHT / 2;
+        int cx = x + WIDTH / 2;
+        int cy = y + HEIGHT / 2;
 
         switch (ptDir) {
             case L:
-                g.drawLine(centerX, centerY, x, centerY);
+                g.drawLine(cx, cy, x, cy);
                 break;
             case LU:
-                g.drawLine(centerX, centerY, x, y);
+                g.drawLine(cx, cy, x, y);
                 break;
             case U:
-                g.drawLine(centerX, centerY, centerX, y);
+                g.drawLine(cx, cy, cx, y);
                 break;
             case RU:
-                g.drawLine(centerX, centerY, x + WIDTH, y);
+                g.drawLine(cx, cy, x + WIDTH, y);
                 break;
             case R:
-                g.drawLine(centerX, centerY, x + WIDTH, centerY);
+                g.drawLine(cx, cy, x + WIDTH, cy);
                 break;
             case RD:
-                g.drawLine(centerX, centerY, x + WIDTH, y + HEIGHT);
+                g.drawLine(cx, cy, x + WIDTH, y + HEIGHT);
                 break;
             case D:
-                g.drawLine(centerX, centerY, centerX, y + HEIGHT);
+                g.drawLine(cx, cy, cx, y + HEIGHT);
                 break;
             case LD:
-                g.drawLine(centerX, centerY, x, y + HEIGHT);
+                g.drawLine(cx, cy, x, y + HEIGHT);
                 break;
-            default:
+            case STOP:
                 break;
         }
 
@@ -70,39 +70,17 @@ public class Tank {
 
     void move() {
         switch (dir) {
-            case L:
-                x -= XSPEED;
-                break;
-            case LU:
-                x -= XSPEED;
-                y -= YSPEED;
-                break;
-            case U:
-                y -= YSPEED;
-                break;
-            case RU:
-                x += XSPEED;
-                y -= YSPEED;
-                break;
-            case R:
-                x += XSPEED;
-                break;
-            case RD:
-                x += XSPEED;
-                y += YSPEED;
-                break;
-            case D:
-                y += YSPEED;
-                break;
-            case LD:
-                x -= XSPEED;
-                y += YSPEED;
-                break;
-            case STOP:
-                break;
+            case L: x -= XSPEED; break;
+            case LU: x -= XSPEED; y -= YSPEED; break;
+            case U: y -= YSPEED; break;
+            case RU: x += XSPEED; y -= YSPEED; break;
+            case R: x += XSPEED; break;
+            case RD: x += XSPEED; y += YSPEED; break;
+            case D: y += YSPEED; break;
+            case LD: x -= XSPEED; y += YSPEED; break;
+            case STOP: break;
         }
 
-        // Ствол танка принимает текущее направление
         if (this.dir != Direction.STOP) {
             this.ptDir = this.dir;
         }
@@ -111,21 +89,10 @@ public class Tank {
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
-            case KeyEvent.VK_CONTROL:
-                tc.m = fire();
-                break;
-            case KeyEvent.VK_LEFT:
-                bL = true;
-                break;
-            case KeyEvent.VK_UP:
-                bU = true;
-                break;
-            case KeyEvent.VK_RIGHT:
-                bR = true;
-                break;
-            case KeyEvent.VK_DOWN:
-                bD = true;
-                break;
+            case KeyEvent.VK_LEFT: bL = true; break;
+            case KeyEvent.VK_UP: bU = true; break;
+            case KeyEvent.VK_RIGHT: bR = true; break;
+            case KeyEvent.VK_DOWN: bD = true; break;
         }
         locateDirection();
     }
@@ -133,18 +100,11 @@ public class Tank {
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
-            case KeyEvent.VK_LEFT:
-                bL = false;
-                break;
-            case KeyEvent.VK_UP:
-                bU = false;
-                break;
-            case KeyEvent.VK_RIGHT:
-                bR = false;
-                break;
-            case KeyEvent.VK_DOWN:
-                bD = false;
-                break;
+            case KeyEvent.VK_CONTROL: fire(); break;
+            case KeyEvent.VK_LEFT: bL = false; break;
+            case KeyEvent.VK_UP: bU = false; break;
+            case KeyEvent.VK_RIGHT: bR = false; break;
+            case KeyEvent.VK_DOWN: bD = false; break;
         }
         locateDirection();
     }
@@ -164,6 +124,8 @@ public class Tank {
     public Missile fire() {
         int mx = this.x + WIDTH / 2 - Missile.WIDTH / 2;
         int my = this.y + HEIGHT / 2 - Missile.HEIGHT / 2;
-        return new Missile(mx, my, ptDir);
+        Missile m = new Missile(mx, my, ptDir);
+        tc.missiles.add(m);
+        return m;
     }
 }
