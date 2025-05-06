0216ce8ff4ecfbeb0b4143cb7958f5aaaccccfb2
Alexan37 <ansuprun98@mail.ru>
2025-05-06 22:49:01 +0800

    Adding a class for projectiles

--- src/com/dong/tank/Tank.java
+++ src/com/dong/tank/Tank.java
@@ -1,19 +1,56 @@
-@Override
-public void keyReleased(KeyEvent e) {
-    int key = e.getKeyCode();
-    switch (key) {
-        case KeyEvent.VK_LEFT:
-            bL = false;
-            break;
-        case KeyEvent.VK_UP:
-            bU = false;
-            break;
-        case KeyEvent.VK_RIGHT:
-            bR = false;
-            break;
-        case KeyEvent.VK_DOWN:
-            bD = false;
-            break;
+import java.awt.*;
+
+public class Missile {
+    public static final int XSPEED = 10;
+    public static final int YSPEED = 10;
+    private int x, y;
+    private Tank.Direction dir;
+
+    public Missile(int x, int y, Tank.Direction dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
     }
-    locateDirection();
+
+    public void draw(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.BLACK);
+        g.fillOval(x, y, 10, 10); // отрисовка снаряда
+        g.setColor(c);
+
+        move(); // движение после отрисовки
+    }
+
+    private void move() {
+        switch (dir) {
+            case L:
+                x -= XSPEED;
+                break;
+            case LU:
+                x -= XSPEED;
+                y -= YSPEED;
+                break;
+            case U:
+                y -= YSPEED;
+                break;
+            case RU:
+                x += XSPEED;
+                y -= YSPEED;
+                break;
+            case R:
+                x += XSPEED;
+                break;
+            case RD:
+                x += XSPEED;
+                y += YSPEED;
+                break;
+            case D:
+                y += YSPEED;
+                break;
+            case LD:
+                x -= XSPEED;
+                y += YSPEED;
+                break;
+        }
+    }
 }
