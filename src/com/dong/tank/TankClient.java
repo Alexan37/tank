aa5d66d4597d49009506a67dbbc32e6966138918
Alexan37 <ansuprun98@mail.ru>
2025-05-06 22:36:10 +0800

    Code refactoring

--- src/com/dong/tank/TankClient.java
+++ src/com/dong/tank/TankClient.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
 public class TankClient extends Frame {
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 600;
     int x = 50, y = 50;
     //这是一张虚拟图片
     Image offScreenImage = null;
@@ -14,21 +16,22 @@ public class TankClient extends Frame {
     }
     public void update(Graphics g) {
         if(offScreenImage == null) {
-            offScreenImage = this.createImage(800, 600);
+            offScreenImage = this.createImage(GAME_WIDTH,
+                    GAME_HEIGHT);
         }
 //拿到这个图片的画笔
         Graphics gOffScreen = offScreenImage.getGraphics();
-        6
         Color c = gOffScreen.getColor();
         gOffScreen.setColor(Color.GREEN);
-        gOffScreen.fillRect(0, 0, 800, 600);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
         gOffScreen.setColor(c);
         print(gOffScreen);
         g.drawImage(offScreenImage, 0, 0, null);
     }
+8
     public void launchFrame() {
         this.setLocation(300, 50);
-        this.setSize(800, 600);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
         this.setTitle("TankWar");
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
