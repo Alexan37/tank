6fc1b6c0b10369cdb3a6ec9cad9cc2e55797d6e1
Alexan37 <ansuprun98@mail.ru>
2025-05-06 22:34:16 +0800

    Tank movement

--- src/com/dong/tank/TankClient.java
+++ src/com/dong/tank/TankClient.java
@@ -1,15 +1,17 @@
 import java.awt.*;
 import java.awt.event.*;
 public class TankClient extends Frame {
-    //paint这个方法不需要被调用，一旦要被重画的时会被自动 调用
+    int x = 50, y = 50;
+//paint这个方法不需要被调用，一旦要被重画的时会被自动 调用
+4
     public void paint(Graphics g) {
         Color c = g.getColor();
         g.setColor(Color.RED);
-        g.fillOval(50, 50, 30, 30);
+        g.fillOval(x, y, 30, 30);
         g.setColor(c);
+        y += 5;
     }
     public void launchFrame() {
-        3
         this.setLocation(300, 50);
         this.setSize(800, 600);
         this.setTitle("TankWar");
@@ -21,9 +23,22 @@ public class TankClient extends Frame {
         this.setResizable(false);
         this.setBackground(Color.GREEN);
         setVisible(true);
+        new Thread(new PaintThread()).start();
     }
     public static void main(String[] args) {
         TankClient tc = new TankClient();
         tc.launchFrame();
+    }
+    private class PaintThread implements Runnable {
+        public void run() {
+            while(true) {
+                repaint();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
