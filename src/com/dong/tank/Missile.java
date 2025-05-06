f302592469b08dae7522e384ed6cb325d4123db5
Alexan37 <ansuprun98@mail.ru>
2025-05-06 23:34:45 +0800

    Destruction of an enemy tank

--- src/com/dong/tank/Missile.java
+++ src/com/dong/tank/Missile.java
@@ -48,13 +48,24 @@ public class Missile {
             case RD: x += XSPEED; y += YSPEED; break;
             case D:  y += YSPEED; break;
             case LD: x -= XSPEED; y += YSPEED; break;
-            case STOP: break;
+            default: break;
         }
 
-        // Удаление снаряда, если он вышел за пределы окна
         if (x < 0 || y < 0 || x > TankClient.GAME_WIDTH || y > TankClient.GAME_HEIGHT) {
             live = false;
-            tc.missiles.remove(this);
         }
+    }
+
+    public Rectangle getRect() {
+        return new Rectangle(x, y, WIDTH, HEIGHT);
+    }
+
+    public boolean hitTank(Tank t) {
+        if (this.live && t.isLive() && this.getRect().intersects(t.getRect())) {
+            t.setLive(false);
+            this.live = false;
+            return true;
+        }
+        return false;
     }
 }
